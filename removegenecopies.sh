a8f58aa72e05f096b6e3f14555ad3a3ff55235d3
Diego Mallo <adamallo@gmail.com>
2017-02-06 23:27:17 +0100

    Added more levels of missing data

--- removegenecopies.sh
+++ removegenecopies.sh
@@ -17,7 +17,9 @@ module load python/2.7.8
 python $BIN/removegenecopies.py . 0.10 r0_10.tree -s 22 -ncores $NSLOTS
 python $BIN/removegenecopies.py . 0.25 r0_25.tree -s 22 -ncores $NSLOTS
 python $BIN/removegenecopies.py . 0.5 r0_50.tree -s 22 -ncores $NSLOTS
+python $BIN/removegenecopies.py . 0.75 r0_75.tree -s 22 -ncores $NSLOTS
 python $BIN/removegenecopies.py -mk byindividual -ist 0.2 -itmin 0 -itmax 1 . 0.10 r1_10.tree -s 22 -ncores $NSLOTS
 python $BIN/removegenecopies.py -mk byindividual -ist 0.2 -itmin 0 -itmax 1 . 0.25 r1_25.tree -s 22 -ncores $NSLOTS
 python $BIN/removegenecopies.py -mk byindividual -ist 0.2 -itmin 0 -itmax 1 . 0.5 r1_50.tree -s 22 -ncores $NSLOTS
+python $BIN/removegenecopies.py -mk byindividual -ist 0.2 -itmin 0 -itmax 1 . 0.75 r1_75.tree -s 22 -ncores $NSLOTS
 #./simphygtrees_collapse.sh ../data/sim1/
